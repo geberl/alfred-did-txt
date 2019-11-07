49905b2d24a6803b06438b30afe274233b327ae1
Günther Eberl <guenther@eberl.se>
2019-11-07 20:08:30 +0100

    Calculate attendance time of day if keywords "arrive"/"leave" used

--- script.py
+++ script.py
@@ -5,6 +5,54 @@ import sys
 
 RESULTS_FILE = "/Users/guenther/Library/Mobile Documents/com~apple~CloudDocs/Documents/did.txt"
 
+
+def calc_attendance_times(separator="---"):
+    with open(file=RESULTS_FILE, mode="r") as file_handler:
+        input_lines = file_handler.readlines()
+
+    output_lines = []
+    last_arrive_datetime = None
+    last_leave_datetime = None
+    attendance_already_calculated = False
+
+    for line in input_lines:
+        if line.strip() == separator:
+            # Calculate attendance time when encountering a separator
+            if last_arrive_datetime != None and last_leave_datetime != None and attendance_already_calculated == False:
+                time_difference = last_leave_datetime - last_arrive_datetime
+                date_string = last_leave_datetime.strftime("%Y-%m-%d")
+                output_lines.append("%s 23:59:59 - attendance (TODO account for breaks) %s" % (date_string, time_difference))
+
+            # Never get rid of the separator
+            output_lines.append(separator)
+
+            # Always reset variables that hold times of previous lines
+            last_arrive_datetime = None
+            last_leave_datetime = None
+            attendance_already_calculated = False
+
+            continue
+
+        line_segments = line.strip().split(" - ")
+        date_string = line_segments[0]
+
+        keyword_string = line_segments[1]
+        this_datetime = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+
+        if keyword_string.startswith("arrive"):
+            last_arrive_datetime = this_datetime
+        elif keyword_string.startswith("leave"):
+            last_leave_datetime = this_datetime
+        elif keyword_string.startswith("attendance"):
+            attendance_already_calculated = True
+    
+        output_lines.append(line.strip())
+    
+    with open(file=RESULTS_FILE, mode="w") as file_handler:
+        for line in output_lines:
+            file_handler.write("%s\n" % line)
+
+
 def format_results(separator="---"):
     with open(file=RESULTS_FILE, mode="r") as file_handler:
         input_lines = file_handler.readlines()
@@ -36,6 +84,7 @@ def append_message(message="n/a"):
     with open(file=RESULTS_FILE, mode="a") as file_handler:
         file_handler.write("%s - %s\n" % (current_datetime.isoformat(" ", "seconds"), message))
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         # The called script itself is always argument 0, so len is always at least 1.
@@ -46,3 +95,4 @@ if __name__ == "__main__":
         print("Only pass one message")
 
     format_results()
+    calc_attendance_times()
